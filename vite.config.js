ee86af719671a9e2a6d3ee2e393475091ce056c1
kartik <kartikc.cse22@sbjit.edu.in>
2025-10-22 22:42:16 +0530

    ok

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,14 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  }
 })
